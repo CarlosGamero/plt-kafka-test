02fe45665e069e3f8c672012804a1db3f491af51
Krzysztof Jamrog <krzysztof.jamrog@lokalise.com>
2025-08-04 17:45:44 +0200

    producer-consumer test

--- test.spec.ts
+++ test.spec.ts
@@ -1,37 +1,57 @@
 import {
-    Consumer,
-    stringDeserializers,
+  Consumer,
+  Producer,
+  stringDeserializers,
+  stringSerializers,
 } from '@platformatic/kafka'
 import { setTimeout } from 'node:timers/promises'
 import {describe, it} from "vitest";
+import {randomUUID} from 'node:crypto';
 
 const bootstrapBrokers = ['localhost:9092']
 
-describe('platformatic-kafka consumer problem', () => {
-    it('test', { timeout: 150000 }, async () => {
-        const clientId = 'my_client'
-        const topic = 'test-topic'
-        const groupId = 'test-group'
+describe('Test consumer', () => {
+  it('test', { timeout: 150000 }, async () => {
+    const topic = 'test-topic'
+    const groupId = 'test-group'
 
-        const consumer = new Consumer({
-            clientId,
-            groupId,
-            bootstrapBrokers,
-            deserializers: stringDeserializers,
-            autocreateTopics: true,
-        })
-
-        const stream = await consumer.consume({ topics: [topic],  })
-        stream.on('data', (message) => {
-            console.log(message)
-        })
-
-        console.log('Consumer active')
-        await setTimeout(100000)
-        console.log('Timeout reached, closing consumer')
-
-        // Cleanup
-        await new Promise((done) => stream.close(done))
-        await consumer.close()
+    const consumer = new Consumer({
+      clientId: randomUUID(),
+      groupId,
+      bootstrapBrokers,
+      deserializers: stringDeserializers,
+      autocreateTopics: true,
     })
+
+    const stream = await consumer.consume({ topics: [topic],  })
+    stream.on('data', async (message) => {
+      console.log('Received message', message)
+      await message.commit()
+    })
+
+    const producer = new Producer({
+      clientId: randomUUID(),
+      bootstrapBrokers,
+      serializers: stringSerializers,
+      autocreateTopics: true,
+    })
+
+    await producer.send({
+      messages: [
+        {
+          topic,
+          key: 'test-key',
+          value: 'Test message 1',
+        },
+      ]
+    })
+
+    await setTimeout(5000)
+    console.log('Timeout reached, closing consumer')
+
+    // Cleanup
+    await stream.close()
+    await consumer.close()
+    await producer.close()
+  })
 })
